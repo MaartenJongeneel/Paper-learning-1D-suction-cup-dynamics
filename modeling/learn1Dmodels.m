45c1969321c8817246c5c37654957f0476683289
Lubbers <m.l.s.lubbers@student.tue.nl>
2022-07-21 20:07:05 +0200

    Cleaned and renamed some scripts
    Former-commit-id: 0753ae6e170844da169481c8f4d2603f17f10441

--- modeling/learn1Dmodels.m
+++ modeling/learn1Dmodels.m
@@ -4,17 +4,8 @@
 % not decreased by more than epsilon over the last 5 training iterations,
 % and a minimum amount of iterations has been reached. Training also stops
 % when a maximum amount of iterations is reached. The progress bar shows an
-% estimate of remaining time untill completion. This estimate is the mean 
-% learning time per model times amount of models still to be learned.
-% This estimate may be way off, and to prevent the estimated time from
-% increasing while learning, it is advised to arrange the init_Dvec from 
-% large to small (large values correspond to small kernel width).
-
+% estimate of remaining time untill completion.
 close all; clear; clc;
-
-% Add paths in order to access functions
-addpath("functions\")
-addpath("functions\LWPR\")
 
 %% Load data and format it to be readable for LWPR algorithm
 load("meanAndStdData.mat")
@@ -33,10 +24,10 @@ masses = sort(unique(masses));
 wb = waitbar(0,'Training..');
 k = 0; % dummy to for waitbar
 
-% Set training parameters
-% init_Dvec = 1000:-100:200; % initial kernel width/receptive field distance metric
-% init_Dvec = 100:-10:20;
-init_Dvec = 1000;
+% init_Dvec = 250:50:350; % initial kernel width/receptive field distance metric
+init_Dvec = 1;
+
+% Convergence criterion settings
 maxIter = 100; % maximum amount of iterations
 minIter = 5; % minimum amount of iterations
 epsilon = 0.0001; % convergence criterion
@@ -51,7 +42,7 @@ for init_D = init_Dvec % initial kernel width hyperparameter loop
         waitbar(x,wb,append(string(round(x*100,1)), "%, estimated time left: ", string(round((trainingBouts - k)*mean(timeLefts)/60,1)), " minutes..."))
         
         % Train a model
-        learnLWPRfuncAverage(data, m, init_D, maxIter, minIter, epsilon) % models are saved within this function
+        learnLWPRfunc(data, m, init_D, maxIter, minIter, epsilon) % models are saved within this function
         
         % Estimate time left
         timeElapsed = toc;
@@ -61,6 +52,3 @@ for init_D = init_Dvec % initial kernel width hyperparameter loop
 end
 close(wb)
 
-%% Remove paths to avoid cluttering the matlab paths
-rmpath("functions\")
-rmpath("functions\LWPR\")
