a93566cafc86f9e1d964a86d4c0e4740825249b5
s140067 <m.j.jongeneel@tue.nl>
2022-08-23 15:28:58 +0200

    Updated to have PlasticPlate mass correctly pulled from data

--- functions/processData.m
+++ functions/processData.m
@@ -63,7 +63,7 @@ for i = 1:Nexps
     processedData.(append("experiment",string(i))).dh = dh;
     processedData.(append("experiment",string(i))).ddh = ddh;
     
-    mass = str2double(exp.OBJECT.PlasticPlate002.attr.mass);
+    mass = str2double(exp.OBJECT.PlasticPlate002.mass.ds);
     g = 9.81;
     f_scuppckg = mass*ddh + mass*g;
     processedData.(append("experiment",string(i))).f_scuppckg = f_scuppckg;
